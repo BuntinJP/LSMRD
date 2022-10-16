07350fac8ea6bf75dd281706f2368b69ea08560d
BuntinWindows <aokitakumi72@gmail.com>
2022-10-17 00:33:36 +0900

    コード成型

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const axios = require("axios");
 const execSync = require("child_process").execSync;
 let date = new Date();
-const URL = ``;
+const URL = "https://discord.com/api/webhooks/1026194723746091038/tfPze97s_1IzO7UDNklZWPcHE4xR-Z3YPn2Ht3X4lbjzNiVk-GQ9yCvuiy8ZDOSetqR0";
 
 //ヘッダーなどの設定
 const config = {
@@ -14,15 +14,15 @@ const config = {
 };
 
 //送信するデータ
-const postData = {
-	username: "OracleLinuxManager",
-	content:
-		date.toLocaleString() +
-		"現在のグローバルIP  ：" +
-		execSync("curl inet-ip.info").toString(),
-};
+function naiyou() {
+	let nd = `${date.toLocaleString()}現在のグローバルIP`;
+	let ip = execSync("curl inet-ip.info").toString();
+	return {
+		content: nd + ip
+	}
+}
+
 const startNoticePost = {
-	
 	content: "IP通知システム起動！！毎日0,6,12,18時に通知します。",
 };
 
@@ -30,7 +30,11 @@ const main = async (post) => {
 	const res = await axios.post(URL, post, config);
 	console.log(res);
 };
+
 main(startNoticePost);
 const cron = require("node-cron");
-const { start } = require("repl");
-cron.schedule("0 0 0,6,12,18 * * *", () => main(postData));
+cron.schedule("0 0 0,6,12,18 * * *", (date) =>{
+	let nd = `${date.toLocaleString()}現在のグローバルIP`;
+	let ip = execSync("curl inet-ip.info").toString();
+	main({content: nd + ip});
+});
