1dec416a1ed9e2137ba0e154d64bd774fffc9cb0
Buntin <aokitakumi72@gmail.com>
2023-03-06 19:21:54 +0900

    remove t.ts

--- src/t.ts
+++
@@ -1,9 +0,0 @@
-import toml from 'toml';
-import fs from 'fs';
-import execSync from 'child_process';
-
-const shell = (command: string): string => {
-    return execSync.execSync(command).toString().trim();
-};
-
-console.log(shell('systemctl status nginx.service'));
